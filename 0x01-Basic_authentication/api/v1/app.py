30d61eb48373f4f57e17094a7c6b18d226b42b4e
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-02-07 11:20:32 +0300

    do basic auth tasks

---
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+App.py
+"""
+from os import getenv
+
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
+from flask import Flask, abort, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.before_request
+def check_requires_auth():
+    """
+    filters requests that need authorization
+    """
+    requires_auth = auth.require_auth(
+        request.path, ['/api/v1/status/',
+                       '/api/v1/unauthorized/',
+                       '/api/v1/forbidden/'])
+    if not requires_auth:
+        return None
+    if not auth.authorization_header(request):
+        abort(401)
+    if not auth.current_user(request):
+        abort(403)
+
+
+@app.errorhandler(404)
+def not_found(e) -> str:
+    """ Handle 404
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(e) -> str:
+    """
+        Handle 401
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(e) -> str:
+    """
+        Handle 403
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
