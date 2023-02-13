b423c8831a1bcdbc6cc5e47458ff6c4c081ac83e
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-02-13 22:53:28 +0300

    login functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,8 @@ Flask app
 """
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
+from sqlalchemy.exc import NoResultFound
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -25,12 +26,39 @@ def register_user():
     email = request.form.get("email")
     password = request.form.get("password")
 
+    if email is None or password is None:
+        return
+
     try:
         AUTH.register_user(email, password)
         return jsonify({"email": "<registered email>",
                         "message": "user created"})
     except ValueError:
         return jsonify(message="email already registered"), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """ Logs in a user
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if email is None or password is None:
+        return
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = None
+    try:
+        session_id = AUTH.create_session(email)
+    except NoResultFound:
+        abort(401)
+
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
 
 
 if __name__ == "__main__":
