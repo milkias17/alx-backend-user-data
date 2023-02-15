4ce3962a60d9e7415fb4ff89ec403b66d88b8e5d
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-02-15 15:18:36 +0300

    add profile page api route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,19 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile")
+def profile():
+    """
+    Profile page api route that returns a json paylod with the user's email
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    return jsonify(email=user.email)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
