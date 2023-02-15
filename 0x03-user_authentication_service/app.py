1aa1b9feedf56eca4d452f6d7f2e642f16279c81
Milkias Yeheyis <66522725+milkias17@users.noreply.github.com>
2023-02-15 15:39:33 +0300

    api route for reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,5 +89,21 @@ def profile():
     return jsonify(email=user.email)
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """
+    Gives the password reset token
+    """
+    email = request.form.get("email")
+    if not email:
+        abort(403)
+
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify(email=email, reset_token=reset_token)
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
